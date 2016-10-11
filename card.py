439847393994c3400ba97008aa158fd4329e9684
scott <scottshuffler@gmail.com>
2016-10-11 17:18:42 -0400

    Royal flush check works

--- card.py
+++ card.py
@@ -43,14 +43,8 @@ def check_hand(hand):
 		if loop_counter <= 4:
 			if behind_works:
 				if behind <= 3:
-					# print("Is " + str(sorted_hand[3][0]) + " == " + str(sorted_hand[(3-behind)][0] - behind))
 					if sorted_hand[3][0] is (sorted_hand[(3-behind)][0] - behind):
-						
 						straight_count += 1
-						# print(sorted_hand[(3-behind)])
-						# print(sorted_hand[3-behind])
-						# print(len(sorted_hand))
-						# print("Index " + str(3-behind))
 						straight_deck.append(sorted_hand[(3-behind)])
 						behind += 1
 						# print("BEHIND")
@@ -58,14 +52,8 @@ def check_hand(hand):
 						behind_works = False
 			if ahead_works:
 				if ahead <= 3:
-					# print("Is " + str(sorted_hand[3][0]) + " == " + str(sorted_hand[(3+ahead)][0] + ahead))
 					if sorted_hand[3][0] is (sorted_hand[(3+ahead)][0] + ahead):
-						
 						straight_count += 1
-						# print("Ahead is: " + str(ahead))
-						# print(sorted_hand[(3+ahead)])
-						# print(len(sorted_hand))
-						# print("Index " + str(3+ahead))
 						straight_deck.append(sorted_hand[(3+ahead)])
 						ahead += 1
 						# print("AHEAD")
@@ -82,6 +70,8 @@ def check_hand(hand):
 	if is_a_straight:
 		sorted_hand = sorted(straight_deck, reverse=True)
 
+	# print(sorted_hand)
+
 	for x in sorted_hand:
 		if x[1] == "SP":
 			spades += 1
@@ -97,18 +87,22 @@ def check_hand(hand):
 	if spades >= 5 or diamonds >= 5 or hearts >= 5 or clubs >= 5:
 		is_a_flush = True
 	if is_a_straight:
-		if is_a_flush:
+		if is_a_flush and sorted_hand[0][0] is 14:
+			#print("ROYAL FLUSH")
+			global royal_flush_counter
+			royal_flush_counter += 1
+		elif is_a_flush:
 			global straight_flush_counter
 			straight_flush_counter += 1
-			print("STRAIGHT FLUSH")
+			#print("STRAIGHT FLUSH")
 		else:
 			global straight_counter 
 			straight_counter += 1
-			print("STRAIGHT")
+			#print("STRAIGHT")
 	elif is_a_flush:
 		global flush_counter 
 		flush_counter += 1
-		print("FLUSH")
+		#print("FLUSH")
 	
 
 	
@@ -123,12 +117,10 @@ def run_test():
 	print("Should be: Straight Flush")
 	check_hand(deck)
 	
-
 	deck = [(2,'HE'),(2,'CL'),(4,'HE'),(5,'CL'),(6,'HE'),(7,'HE'),(8,'CL')]
 	print("Should be: Straight")
 	check_hand(deck)
 	
-
 	deck = [(10,'HE'),(2,'CL'),(4,'CL'),(9,'CL'),(6,'CL'),(7,'HE'),(11,'CL')]
 	print("Should be: Flush")
 	check_hand(deck)
@@ -137,7 +129,7 @@ def run_test():
 
 def run_loop():
 	deck = copy_cards()
-	for x in xrange(1,100):
+	for x in xrange(1,100000):
 		hand_results = []
 		for x in range(0,7):
 		    random.shuffle(deck)
@@ -148,13 +140,15 @@ def run_loop():
 		deck = copy_cards()
 	global flush_counter
 	global straight_counter
+	global royal_flush_counter
+	global straight_flush_counter
 	percent_flush = flush_counter / 100000
 	percent_straight = straight_counter / 100000
-	print("Flush: " + str(flush_counter) + " Percent: " + str(percent_flush) + " Straights: " + str(straight_counter) + " Percent: " + str(percent_straight))
+	print("Flush: " + str(flush_counter) + " Straights: " + str(straight_counter) + " Royal flush: " + str(royal_flush_counter) + " Straight flush: " + str(straight_flush_counter))
 
 def main():
-	#run_loop()
-	run_test()
+	run_loop()
+	# run_test()
 
 if __name__ == '__main__':
     main()
